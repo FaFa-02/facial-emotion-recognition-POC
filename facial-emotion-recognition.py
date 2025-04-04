aa825dc9d402a73db9f9291c3c846bba6407d011
FaFa-02 <fabioeugenio.doria@gmail.com>
2025-04-04 11:24:36 +0100

    feat: setup mediapipe task face_landmarker and run on image

--- facial-emotion-recognition.py
+++ facial-emotion-recognition.py
@@ -1,6 +1,24 @@
 import cv2
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
 
-image = cv2.imread('smiling-woman.jpg')
+# def model path
+model_path = 'C:\\Users\\fabot\\Downloads\\face_landmarker.task'
 
-cv2.imshow('Image', image)
-cv2.waitKey(0)
+# Read Images
+image = mp.Image.create_from_file('smiling-woman.jpg')
+
+# Set configuration options
+BaseOptions = mp.tasks.BaseOptions
+FaceLandmarker = mp.tasks.vision.FaceLandmarker
+FaceLandmarkerOptions = mp.tasks.vision.FaceLandmarkerOptions
+VisionRunningMode = mp.tasks.vision.RunningMode
+options = FaceLandmarkerOptions(
+    base_options=BaseOptions(model_asset_path=model_path),
+    running_mode=VisionRunningMode.IMAGE
+)
+
+# Initialise landmarker
+with FaceLandmarker.create_from_options(options) as landmarker:
+    face_landmarker_result = landmarker.detect(image)
